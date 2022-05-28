aceb11c8ec4d584d00b131666bd7761d121caa23
tenderpork <14340342+tenderpork@users.noreply.github.com>
2022-05-28 19:02:49 +0000

    Sat, May 28, 2022, 7:02 PM +00:00

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -43,7 +43,7 @@ def search(request):
 
 class NewEntryForm(forms.Form):
     # title = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Title", "class": "mb-4 mt-3 form-control w-25"}), label="")
-    title = forms.CharField
+    title = forms.CharField(label="")
     markdown = forms.CharField(widget=forms.Textarea(attrs={"class": "d-block form-control w-75 mb-4"}), label="")
 
 def add(request):
