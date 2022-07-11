8f6ff978db0cb8e395628108efed5d6d2371202f
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2022-07-11 13:36:37 -0400

    send email

--- project3/mail/static/mail/inbox.js
+++ project3/mail/static/mail/inbox.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +32,26 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+  const recipients = document.querySelector('#compose-recipients');
+  const subject = document.querySelector('#compose-subject');
+  const body = document.querySelector('#compose-body')
+
+  const response = fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients.value,
+      subject: subject.value,
+      body: body.value
+    })
+  })
+
+  if (response.status === 201) {
+    recipients.value = '';
+    subject.value = '';
+    body.value = '';
+    load_mailbox('inbox');
+  }
 }
